9ec156d73494c6e93571b1fa6874bd3bad18ed1f
kelvin <hanqi_kelvin@163.com>
2021-03-24 12:01:27 +0800

    master: 2

--- src/main/java/com/yykj/git/TestMain.java
+++ src/main/java/com/yykj/git/TestMain.java
@@ -7,6 +7,8 @@ public class TestMain {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Hello Git 2 !!!");
+        System.out.println("10");
     }
+
+
 }
